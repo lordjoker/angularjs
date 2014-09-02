1d0911b7688f22d8ecb173da99a934f32add8248
Marcin Wiśniewski <m.wisniewski@crb24.pl>
2014-09-02 15:44:46 +0200

    filtr + form

--- controller.js
+++ controller.js
@@ -1,6 +1,5 @@
-var mainModule = angular.module('MainModule', []);
-
-mainModule.controller('CartController', 
+var indexModule = angular.module('IndexModule', []);
+indexModule.controller('IndexController', 
 		function($scope) {
 			$scope.items = [
 			                {name: "Item1", price: 2.95, quantity: 5},
@@ -13,3 +12,42 @@ mainModule.controller('CartController',
 			};
 		}
 );
+
+
+
+
+var formModule = angular.module('FormModule', []);
+formModule.controller('FormController',
+		function($scope) {
+			$scope.computeNeeded = function() {
+				$scope.needed = $scope.startingEstimate * 10;
+			};
+			$scope.requestFunding = function() {
+				window.alert("Sorry, please get more customers first");
+			};
+			$scope.reset = function() {
+				$scope.startingEstimate = 0;
+			};
+		}
+);
+
+
+
+
+var filterModule = angular.module('FilterModule', []);
+filterModule.controller('FilterController',
+		function($scope) {
+			$scope.items = [
+			                {name: "Name 1"},
+			                {name: "Name 11"},
+			                {name: "Name 22"}
+			                ];
+		}
+);
+filterModule.filter('boldStyle', function($sce) {
+		var boldStyleFilter = function(input) {
+			var words = input.split(' ');
+			return $sce.trustAsHtml("<b>" + words[0] + "</b> " + words[1]);
+		}
+		return boldStyleFilter;
+});
